39efe164531caeec6c72f81121ac5860add51194
ggsma <ggsma@DESKTOP-B862BGE>
2022-01-25 01:17:27 +0530

    Add project files.

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace ACM.BLTest
+    {
+        [TestClass]
+        public class CustomerTest
+        {
+            [TestMethod]
+            public void FullNameTestValid()
+            {
+                //-- Arrange
+                // var customer = new Customer();
+                Customer customer = new Customer
+                {
+                    FirstName = "Bildo",
+                    LastName = "Baggins"
+                };
+
+                string expected = "Baggins, Bildo";
+
+                //-- Act
+                string actual = customer.FullName;
+
+                //-- Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+
+            [TestMethod]
+            public void FullNameFirstNameEmpty()
+            {
+                //-- Arrange
+
+                Customer customer = new Customer
+                {
+                    LastName = "Baggins"
+                };
+                string expected = "Baggins";
+
+                //-- Act
+                string actual = customer.FullName;
+
+
+                //-- Assert
+                Assert.AreEqual(expected, actual);
+
+            }
+
+
+
+
+
+
+        }
+    }
---
+++ Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a0c8fc33-a069-4c9b-a68a-0341c08892e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
